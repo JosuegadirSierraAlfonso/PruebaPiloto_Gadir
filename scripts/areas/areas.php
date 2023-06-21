9ea9f7759ea654b3e111c4b02ba229c4a272c423
JosuegadirSierraAlfonso <josuegsierraa18@gmail.com>
2023-06-21 09:23:14 -0500

    feat: :construction: Create areas.php
    in progress

---
+++ scripts/areas/areas.php
@@ -0,0 +1,49 @@
+<?php
+
+
+
+
+
+
+/* class areas extends connect{
+    private $query = 'INSERT INTO tb_client(Identification,Full_Name,Email,Address,Phone) VALUES(:cc,:name,:email,:address,:cellphone)';
+    private $queryGetAll = 'SELECT Identification as "cc",full_name,email,address,phone FROM tb_client';
+    private $message;
+    use getInstance;
+    function __construct(
+        private $Identification, 
+        public $Full_Name, 
+        public $Email, 
+        private $Address, 
+        private $Phone){
+        parent::__construct();
+    }
+    private function postClient(){
+        try {
+            $res = $this->conx->prepare($this->query);
+            $res->bindValue("email", $this->Email);
+            $res->bindValue("cc", $this->Identification);
+            $res->bindValue("name", $this->Full_Name);
+            $res->bindValue("address", $this->Address);
+            $res->bindValue("cellphone", $this->Phone);
+            $res->execute();
+            $message = ["Code"=> 200+$res->rowCount(), "Message"=> "Inserted data"];
+        } catch (\PDOException $e){
+            $message = ["Code"=> $e->getCode(), "Message"=> $res->errorInfo()[2]];
+        }finally{
+            print_r($this->message);
+        }
+    }
+    public function getAllClient(){
+        try {
+            $res = $this->conx->prepare($this->queryGetAll);
+            $res->execute();
+            $this->message = ["Code"=> 200, "Message"=> $res->fetchAll(PDO::FETCH_ASSOC)];
+        } catch(\PDOException $e) {
+            $this->message = ["Code"=> $e->getCode(), "Message"=> $res->errorInfo()[2]];
+        }finally{
+            print_r($this->message);
+        }
+    }
+} */
+?>  
--- scripts/db/db_campus.sql
+++ scripts/db/db_campus.sql
@@ -1,3 +1,3 @@
 USE campusland;
 
-SELECT * FROM teachers;
+SELECT * FROM areas;
